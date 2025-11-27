a0ee523be78c2943286f425ec2c8736bb24a1180
Sallai András <andrewteki@gmail.com>
2025-11-27 12:25:33 +0100

    A crop CRUD műveletek működik

--- src/app/crop/crop.component.ts
+++ src/app/crop/crop.component.ts
@@ -1,20 +1,37 @@
 import { Component } from '@angular/core';
 import { CropApi } from '../shared/crop-api';
+import { ProducerApi } from '../shared/producer-api';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-crop',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './crop.component.html',
   styleUrl: './crop.component.css',
 })
 export class CropComponent {
 
   crops: any;
+  producers: any;
+  addMode = true;
+  cropForm: any;
 
-  constructor(private cropApi: CropApi) { }
+  constructor(
+    private cropApi: CropApi,
+    private producerApi: ProducerApi,
+    private builder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.cropForm = this.builder.group({
+      id: '',
+      name: '',
+      type: '',
+      proteinContent: '',
+      producerId: ''
+    })
     this.getCrops()
+    this.getProducers()
   }
 
   getCrops() {
@@ -26,5 +43,72 @@ export class CropComponent {
         console.log(err)
       }
     })
+  }
+  getProducers() {
+    this.producerApi.getProducers().subscribe({
+      next: (res: any) => {
+        this.producers = res.data
+      },
+      error: (err: any) => {
+        console.log(err)
+      }
+    })
+  }
+
+  save() {
+    if(this.addMode) {
+      this.startCreateCrop()
+    } else {
+      this.startUpdateCrop()
+    }
+  }
+  startCreateCrop() {
+    console.log(this.cropForm.value)
+    this.cropApi.createCrop(this.cropForm.value).subscribe({
+      next: (res: any) => {
+        this.getCrops()
+        this.cropForm.reset()
+        console.log(res)
+      },
+      error: (err: any) => {
+        console.log(err)
+      }
+    })
+  }
+  startUpdateCrop() {
+    console.log('Update ...')
+    console.log(this.cropForm.value)
+    this.cropApi.updateCrop(this.cropForm.value).subscribe({
+      next: (res: any) => {
+        this.getCrops()
+        this.cropForm.reset()
+        console.log(res)
+        this.addMode = true
+      },
+      error: (err: any) => {
+        console.log(err)
+      }
+    })
+  }
+
+  startEditCrop(crop: any) {
+    this.addMode = false
+    this.cropForm.patchValue(crop)
+  }
+
+  startDeleteCrop(id: number) {
+    this.cropApi.deleteCrop(id).subscribe({
+      next: (res: any) => {
+        this.getCrops()
+        console.log(res)
+      },
+      error: (err: any) => {
+        console.log(err)
+      }
+    })
+  }
+  cancel() {
+    this.cropForm.reset()
+    this.addMode = true
   }
 }
